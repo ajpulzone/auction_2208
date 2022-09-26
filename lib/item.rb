6c1b758df4f3064806458535409a4dd0df89c93a
Annie Pulzone <ajpulzone@gmail.com>
2022-09-26 09:00:03 -0700

    Feature: current_high_bid method complete, test passed

--- lib/item.rb
+++ lib/item.rb
@@ -6,5 +6,15 @@ class Item
     @bids = Hash.new(0)
   end
 
+  def add_bid(attendee, bid_amount)
+    bids[attendee] += bid_amount
+  end
 
+  def current_high_bid
+    item_bid_values = []
+    bids.each do |attendee, bid_amount|
+      item_bid_values << bid_amount
+    end
+    item_bid_values.max
+  end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -8,7 +8,12 @@ RSpec.describe Auction do
   before(:each) do
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
     @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
     @auction = Auction.new
   end
 
@@ -37,6 +42,37 @@ RSpec.describe Auction do
       @auction.add_item(@item2)
       expect(@auction.items).to eq([@item1, @item2])
       expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+    end
+  end
+
+  describe "#add_bid" do
+    it "adds an attendee and their bid to an items's bid hash" do
+      @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      expect(@item1.bids).to eq({})
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
+    end
+  end
+
+  describe "#current_high_bid" do
+    it "returns the highest value that has been bidded on an item" do
+      @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      expect(@item1.bids).to eq({})
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      expect(@item1.current_high_bid).to eq(22)
+
     end
   end
 end
